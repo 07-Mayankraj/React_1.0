8d05fcc922d4f73f028ca05498b92cecdd80eef2
07-Mayankraj <2003.mayank.raj@gmail.com>
2024-08-14 03:39:26 +0530

    deplyed on netlify

--- use-state-manage/src/Components/Posts.jsx
+++ use-state-manage/src/Components/Posts.jsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
+import { useState , useEffect } from 'react'
+import '../App.css';
 function Posts() {
     const [posts, setPosts] = useState([])
+    const [isLoading ,setIsLoading]  = useState(false)
+    const [isErrorAccured ,setisErrorAccured]  = useState(false)
+    const [page , setPage] = useState(1)
 
-    const getPostData = async () => {
+    const getPostData = async (page) => {
        
         try {
-            const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
+            const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`)
             const data = await response.json()
-            console.log(data)
-            setPosts(data) 
+            return data
         } catch (error) {
-            alert(error.message)            
+            setisErrorAccured(true)
+            console.error(error.message)            
         }
     }
-    console.log({posts}) ;
 
+    const fetchAndUpdateData = async (page) =>{
+        setIsLoading(true)
+        try {
+            let data = await getPostData(page)
+            setPosts(data)
+            setIsLoading(false)
+    
+        } catch (error) {
+            console.error(error)            
+        }
+    }
+
+
+    useEffect(()=>{
+        fetchAndUpdateData(page)
+
+    },[page])
+
+    const handlePageChange = (change) => {
+        setPage(page + change)
+    }
+
+    if(isLoading) return <h1 className='loaders'>Loooooooooooooooading....  :) </h1>
+    if(isErrorAccured) return <h1 className='loaders'>Error while getting the data...  :( </h1>
 
     return <>
 
-        <button onClick={getPostData}> Get Posts</button>
         <ul>
             {
                 posts.map(post => {
-                    return <div>
-                        <li>Id : {post.id}</li>
-                        <li>Title : {post.title}</li>
-                        <li>Body : {post.body}</li>
+                    return <div className='Post-class' >
+                        <li> {post.id}</li>
+                        <li> {post.title}</li>
+                        <li>{post.body}</li>
                     </div>
                 })
             }
         </ul>
+        <div className='pages-div'>
+            <button disabled ={page ===1} onClick={()=>handlePageChange(-1)}>Prev</button>
+            <button>{page}</button>
+            <button onClick={()=>handlePageChange(+1)}>Next</button>
+        </div>
     </>
 }
 
