3da02d6914efdf523d70fe94b352304757d8a353
07-Mayankraj <2003.mayank.raj@gmail.com>
2024-08-25 04:05:03 +0530

    added delete prod button

--- day-6-ecom-prod/src/components/Product.jsx
+++ day-6-ecom-prod/src/components/Product.jsx
@@ -1,10 +1,27 @@
+import toast from "react-hot-toast"
+
+const baseUrl = `https://mock-server-qkzm.onrender.com/products`
 function Product(props) {
     const product = props.product
+
+    const deleteProduct = async() => {
+        try {
+            let response = await fetch(`${baseUrl}/${product.id}`, {
+                method: 'DELETE',
+            })
+            if (response.ok) {
+                toast.success('Product deleted successfully')
+            }
+        } catch (error) {
+            toast.error('Error while deleting the product')
+        }
+    }
 
     return <div className="product" id={product.id}>
         <img src={product.imageUrl} />
         <h3>{product.title}</h3>
         <p>{product.price}</p>
+        <button onClick={deleteProduct}>Delete</button>
 
     </div>
 }
